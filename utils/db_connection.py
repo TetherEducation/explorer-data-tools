04e19c000a310881ad555803889dce20284ac9a8
lejgomezdu <69878323+lejgomezdu@users.noreply.github.com>
2025-02-03 23:05:38 +0100

    Revert "loading NH users for "organic" intervention"

--- utils/db_connection.py
+++ utils/db_connection.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 import psycopg2
 
-#Funcion para conectarse a la base de datos core
+#Funcion para conectarse a la base de datos
 def conect_bd(database, environment):
     if environment == 'staging':
         host = "staging.cluster-cuasuehzhaay.us-east-1.rds.amazonaws.com"
@@ -15,16 +15,5 @@ def conect_bd(database, environment):
         password = os.getenv("db_password"),
         port = os.getenv("db_port"),
         host = host
-    )
-    return conn
-
-#Funcion para conectarse a la base de datos de users de prod
-def conect_bd_users_prod(database):
-    conn = psycopg2.connect(
-        database = database,
-        user = os.getenv("users_user"),
-        password = os.getenv("users_password"),
-        port = "5432",
-        host = "users.cluster-cuasuehzhaay.us-east-1.rds.amazonaws.com"
     )
     return conn
